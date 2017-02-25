6268ebbb21abe4d14fdefaaff16f6c28f5e971c2
Jacky Tang <jackytck@gmail.com>
2017-02-26 01:02:01 +0800

    :hammer: vendor + chunkhash + webpack-dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const VENDOR_LIBS = [
+  'react',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk',
+  'faker'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new DashboardPlugin()
+  ]
 };
